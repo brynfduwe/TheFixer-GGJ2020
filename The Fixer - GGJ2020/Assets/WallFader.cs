be0ac1cc68ea664a776d3b517c9913212e84d12a
brynfduwe <bryn2.findlay-dykes@live.uwe.ac.uk>
2020-02-01 23:51:37 +0000

    wall fadering

---
+++ The Fixer - GGJ2020/Assets/WallFader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallFader : MonoBehaviour
+{
+    [SerializeField] SpriteRenderer m_sprite = null;
+    [SerializeField] Transform m_playerRef = null;
+    Color m_startColor = Color.white;
+
+    private void Start()
+    {
+        m_startColor = m_sprite.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(m_playerRef.position.z >= transform.position.z)
+        {
+            m_sprite.color = Color.Lerp(m_sprite.color, Color.clear, Time.deltaTime * 2);
+        }
+        else
+        {
+            m_sprite.color = Color.Lerp(m_sprite.color, m_startColor, Time.deltaTime * 2);
+        }
+    }
+}
