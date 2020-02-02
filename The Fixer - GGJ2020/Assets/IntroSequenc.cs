7e8e292c4cafce9ab367f699eb69bd18c24d03c9
brynfduwe <bryn2.findlay-dykes@live.uwe.ac.uk>
2020-02-02 12:15:04 +0000

    Intro sequence yayaya

--- The Fixer - GGJ2020/Assets/IntroSequenc.cs
+++ The Fixer - GGJ2020/Assets/IntroSequenc.cs
@@ -11,53 +11,62 @@ public class IntroSequenc : MonoBehaviour
     [SerializeField] Image m_phoneLowered = null;
     [SerializeField] Image m_phoneRaised = null;
 
+    float startLerp = 0;
+    bool end = false;
+    float endLerp = 0;
+
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(StartIntro());
     }
 
+    private void Update()
+    {
+        if (startLerp < 1)
+        {
+            startLerp += Time.deltaTime * 1f;
+            m_phoneLowered.color = Color.Lerp(Color.clear, Color.gray, startLerp);
+        }
+        else
+        {
+            if (end)
+            {
+                if (endLerp < 1)
+                {
+                    endLerp += Time.deltaTime * 0.05f;
+                    m_phoneLowered.color = Color.Lerp(Color.gray, Color.clear, startLerp);
+                }
+            }
+        }
+    }
+
+
     IEnumerator StartIntro()
     {
-        float lerp1 = 0;
-        while(lerp1 < 1)
-        {
-            lerp1 += Time.deltaTime;
-            m_phoneLowered.color = Color.Lerp(Color.clear, Color.gray, lerp1);
-        }
-
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(3);
         //RING RING RING
-        float lerp2 = 0;
-        while (lerp2 < 1)
-        {
-            lerp2 += Time.deltaTime;
-            m_phoneLowered.color = Color.Lerp(Color.gray, Color.clear, lerp2);
-            m_phoneRaised.color = Color.Lerp(Color.clear, Color.gray, lerp2);
-        }
-        UIManager.instance.NewSubtitle("The Boss", "The goons, they fucked the job, you gotta get over there and fix this mess.", 5);
-        yield return new WaitForSeconds(5);
+        m_phoneLowered.color = Color.clear;
+        m_phoneRaised.color = Color.gray;
+        yield return new WaitForSeconds(2);
+
+        UIManager.instance.NewSubtitle("The Boss", "The goons, they screwed the job, you gotta get over there and fix this mess.", 7);
+        yield return new WaitForSeconds(7);
         UIManager.instance.NewSubtitle("The Fixer", "...where?", 3);
         yield return new WaitForSeconds(3);
         UIManager.instance.NewSubtitle("The Boss", "Docks, Morgan and Son’s warehouse. The cop’s are already on the way.", 5);
         yield return new WaitForSeconds(5);
         UIManager.instance.NewSubtitle("The Boss", "You got 10 minutes. Get it done.", 4);
-        float lerp3 = 0;
+        yield return new WaitForSeconds(5);
+
+        m_phoneLowered.color = Color.gray;
+        m_phoneRaised.color = Color.clear;
+
         yield return new WaitForSeconds(2);
-        while (lerp3 < 1)
-        {
-            lerp3 += Time.deltaTime;
-            m_phoneLowered.color = Color.Lerp(Color.clear, Color.gray, lerp3);
-            m_phoneRaised.color = Color.Lerp(Color.gray, Color.clear, lerp3);
-        }
-        yield return new WaitForSeconds(2);
-        float lerp4 = 0;
-        while (lerp4 < 1)
-        {
-            lerp4 += Time.deltaTime;
-            m_phoneLowered.color = Color.Lerp(Color.gray, Color.clear, lerp4);
-        }
-        yield return new WaitForSeconds(2);
+
+        end = true;
+
+        yield return new WaitForSeconds(3f);
 
         m_timer.enabled = true;
         m_musicManager.Init();
