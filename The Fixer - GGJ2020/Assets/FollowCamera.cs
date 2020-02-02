6593e2dc8eba8aea719d063d96bd686c67de1998
brynfduwe <bryn2.findlay-dykes@live.uwe.ac.uk>
2020-02-02 08:33:11 +0000

    Dialog twewaks, some other little tweaks too

---
+++ The Fixer - GGJ2020/Assets/FollowCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] Transform m_target;
+    [SerializeField] Vector3 m_offset;
+    [SerializeField] float m_speed;
+
+    private void Start()
+    {
+        m_offset = transform.position - m_target.position;
+    }
+
+    void Update()
+    {
+        if (m_target != null)
+        {
+            transform.position = Vector3.Lerp(transform.position, m_target.position + m_offset, m_speed * Time.deltaTime);
+        }
+    }
+}
